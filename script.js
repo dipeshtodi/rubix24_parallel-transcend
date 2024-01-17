282a5130f51ff77222f682b4c39c66239684d887
Vinayak <96956957+Vinayak1802@users.noreply.github.com>
2024-01-18 04:15:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+document.getElementById('approvalForm').addEventListener('submit', function(event) {
+            
+    event.preventDefault();
 
+});
+
+function removeFileInput(fileInputId) {
+    document.getElementById(fileInputId).value = null;
+}
+
+document.getElementById('removePhoto').addEventListener('click', function() {
+    removeFileInput('photo');
+});
+
+document.getElementById('removeMedicalCertificate').addEventListener('click', function() {
+    removeFileInput('medicalCertificate');
+});
+
+document.getElementById('removeBirthCertificate').addEventListener('click', function() {
+    removeFileInput('birthCertificate');
+});
+
+//landing page
+document.addEventListener('DOMContentLoaded', function () {
+    const faqButtons = document.querySelectorAll('.faq-button');
+
+    faqButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            const answer = button.nextElementSibling;
+
+            // Toggle the visibility of the answer
+            answer.classList.toggle('hidden');
+        });
+    });
+});
